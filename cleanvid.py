aa784fa5248c098867c65d98d0408873d57d5ef1
SG <13872653+mmguero@users.noreply.github.com>
2020-08-25 15:58:10 -0600

    fix issue #3, hopefully, by normalizing sub files to non-BOM UTF-8 before use

--- cleanvid.py
+++ cleanvid.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 import argparse
+import chardet
+import codecs
 import errno
 import os
+import shutil
 import sys
 import re
 import pysrt
@@ -30,11 +33,38 @@ def GetSubtitles(vidFileSpec, srtLanguage):
 
   return subFileSpec
 
+######## UTF8Convert #########################################################
+# attempt to convert any text file to UTF-* without BOM and normalize line endings
+def UTF8Convert(fileSpec, universalEndline=True):
+
+  # Read from file
+  with open(fileSpec, 'rb') as f:
+    raw = f.read()
+
+  # Decode
+  raw = raw.decode(chardet.detect(raw)['encoding'])
+
+  # Remove windows line endings
+  if universalEndline:
+    raw = raw.replace('\r\n', '\n')
+
+  # Encode to UTF-8
+  raw = raw.encode('utf8')
+
+  # Remove BOM
+  if raw.startswith(codecs.BOM_UTF8):
+    raw = raw.replace(codecs.BOM_UTF8, '', 1)
+
+  # Write to file
+  with open(fileSpec, 'wb') as f:
+    f.write(raw)
+
 #################################################################################
 class VidCleaner(object):
   inputVidFileSpec = ""
   inputSubsFileSpec = ""
   cleanSubsFileSpec = ""
+  tmpSubsFileSpec = ""
   outputVidFileSpec = ""
   swearsFileSpec = ""
   swearsMap = CaselessDictionary({})
@@ -45,8 +75,7 @@ class VidCleaner(object):
     if os.path.isfile(iVidFileSpec):
       self.inputVidFileSpec = iVidFileSpec
     else:
-      raise IOError(
-          errno.ENOENT, os.strerror(errno.ENOENT), iVidFileSpec)
+      raise IOError(errno.ENOENT, os.strerror(errno.ENOENT), iVidFileSpec)
 
     if os.path.isfile(iSubsFileSpec):
       self.inputSubsFileSpec = iSubsFileSpec
@@ -54,8 +83,7 @@ class VidCleaner(object):
     if os.path.isfile(iSwearsFileSpec):
       self.swearsFileSpec = iSwearsFileSpec
     else:
-      raise IOError(
-          errno.ENOENT, os.strerror(errno.ENOENT), iSwearsFileSpec)
+      raise IOError(errno.ENOENT, os.strerror(errno.ENOENT), iSwearsFileSpec)
 
     self.outputVidFileSpec = oVidFileSpec
     if os.path.isfile(self.outputVidFileSpec):
@@ -65,10 +93,17 @@ class VidCleaner(object):
   def __del__(self):
     if os.path.isfile(self.cleanSubsFileSpec) and (not os.path.isfile(self.outputVidFileSpec)):
       os.remove(self.cleanSubsFileSpec)
+    if os.path.isfile(self.tmpSubsFileSpec):
+      os.remove(self.tmpSubsFileSpec)
 
   ######## CreateCleanSubAndMuteList #################################################
   def CreateCleanSubAndMuteList(self, cleanSubsFileSpec=None):
     subFileParts = os.path.splitext(self.inputSubsFileSpec)
+
+    self.tmpSubsFileSpec = subFileParts[0] + "_utf8" + subFileParts[1]
+    shutil.copy2(self.inputSubsFileSpec, self.tmpSubsFileSpec)
+    UTF8Convert(self.tmpSubsFileSpec)
+
     if cleanSubsFileSpec is not None:
       self.cleanSubsFileSpec = cleanSubsFileSpec
     else:
@@ -88,7 +123,7 @@ class VidCleaner(object):
 
     replacer = re.compile(r'\b(' + '|'.join(self.swearsMap.keys()) + r')\b', re.IGNORECASE)
 
-    subs = pysrt.open(self.inputSubsFileSpec)
+    subs = pysrt.open(self.tmpSubsFileSpec)
     newSubs = pysrt.SubRipFile()
     for sub in subs:
       newText = replacer.sub(lambda x: self.swearsMap[x.group()], sub.text)
@@ -110,14 +145,19 @@ class VidCleaner(object):
 
   ######## MultiplexCleanVideo ###################################################
   def MultiplexCleanVideo(self):
-    ffmpegResult = delegator.run("ffmpeg -y -i \"" + self.inputVidFileSpec + "\"" + \
-                    " -c:v copy " + \
-                    " -af \"" + ",".join(self.muteTimeList) + "\"" \
-                    " -c:a aac -ac 2 -ab 224k -ar 44100 \"" + \
-                    self.outputVidFileSpec + "\"", block=True)
-    if (ffmpegResult.return_code != 0) or (not os.path.isfile(self.outputVidFileSpec)):
-      print(ffmpegResult.err)
-      raise ValueError('Could not process %s' % (self.inputVidFileSpec))
+    if len(self.muteTimeList) > 0:
+      ffmpegCmd = "ffmpeg -y -i \"" + self.inputVidFileSpec + "\"" + \
+                      " -c:v copy " + \
+                      " -af \"" + ",".join(self.muteTimeList) + "\"" \
+                      " -c:a aac -ac 2 -ab 224k -ar 44100 \"" + \
+                      self.outputVidFileSpec + "\""
+      ffmpegResult = delegator.run(ffmpegCmd, block=True)
+      if (ffmpegResult.return_code != 0) or (not os.path.isfile(self.outputVidFileSpec)):
+        print(ffmpegCmd)
+        print(ffmpegResult.err)
+        raise ValueError(f'Could not process {self.inputVidFileSpec}')
+    else:
+      shutil.copy2(self.inputVidFileSpec, self.outputVidFileSpec)
 
 #################################################################################
 
