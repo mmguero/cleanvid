c856b44967f65229c67d3a054c4ad53442384b10
SG <13872653+mmguero@users.noreply.github.com>
2021-12-31 08:12:27 -0700

    v1.0.1 release with updated scripts and README

--- src/cleanvid/__init__.py
+++ src/cleanvid/__init__.py
@@ -1,6 +1,6 @@
 """cleanvid is a little script to mute profanity in video files."""
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "Seth Grover <mero.mero.guero@gmail.com>"
 __all__ = []
 
