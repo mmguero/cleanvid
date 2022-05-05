7fcd9267c98fe49336b0a8bb48f89a4b890a8ed5
Seth Grover <mero.mero.guero@gmail.com>
2022-05-05 10:22:25 -0600

    more container-engine agnostic

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+IMAGE="${CLEANVID_IMAGE:-ghcr.io/mmguero/cleanvid:latest}"
+ENGINE="${CONTAINER_ENGINE:-docker}"
+
 set -e
 set -o pipefail
 set -u
@@ -8,12 +11,12 @@ ENCODING="utf-8"
 
 [[ "$(uname -s)" = 'Darwin' ]] && REALPATH=grealpath || REALPATH=realpath
 [[ "$(uname -s)" = 'Darwin' ]] && DIRNAME=gdirname || DIRNAME=dirname
-if ! (type "$REALPATH" && type "$DIRNAME" && type docker) > /dev/null; then
-  echo "$(basename "${BASH_SOURCE[0]}") requires docker, $REALPATH and $DIRNAME"
+if ! (type "$REALPATH" && type "$DIRNAME" && type $ENGINE) > /dev/null; then
+  echo "$(basename "${BASH_SOURCE[0]}") requires $ENGINE, $REALPATH and $DIRNAME"
   exit 1
 fi
 export SCRIPT_PATH="$($DIRNAME $($REALPATH -e "${BASH_SOURCE[0]}"))"
 
 pushd "$SCRIPT_PATH"/.. >/dev/null 2>&1
-docker build -f docker/Dockerfile -t ghcr.io/mmguero/cleanvid:latest .
+$ENGINE build -f docker/Dockerfile -t "${IMAGE}" .
 popd >/dev/null 2>&1
