954da45605924681b10917858a79a276dd2fb994
Seth Grover <mero.mero.guero@gmail.com>
2022-05-04 22:31:46 -0600

    allow running cleanvid in docker or podman

--- docker/cleanvid-docker.sh
+++ docker/cleanvid-docker.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-export CLEANVID_IMAGE="${CLEANVID_DOCKER_IMAGE:-ghcr.io/mmguero/cleanvid:latest}"
+IMAGE="${CLEANVID_IMAGE:-ghcr.io/mmguero/cleanvid:latest}"
+ENGINE="${CONTAINER_ENGINE:-docker}"
 
 # run from directory containing video/srt files
 
-docker run --rm -t \
-  -u $(id -u):$(id -g) \
+"${ENGINE}" run --rm -t \
+  -u $([[ "$ENGINE" == "podman" ]] && echo 0 || id -u):$([[ "$ENGINE" == "podman" ]] && echo 0 || id -g) \
   -v "$(realpath "${PWD}"):${PWD}" \
   -w "${PWD}" \
-  "$CLEANVID_IMAGE" "$@"
+  "${IMAGE}" "$@"
